208e8dc676cf73a5ea684ac691261d122f398e79
Ishil Puri <ishil.puri@berkeley.edu>
2022-06-26 22:38:24 -0700

    v1 MVP working

--- tether/AppDelegate.swift
+++ tether/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import UserNotifications
+import BackgroundTasks
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForPushNotifications()
+        registerBackgroundTasks()
         return true
     }
 
@@ -30,7 +34,109 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("App entered background")
+//        let content = UNMutableNotificationContent()
+//        content.title = NSString.localizedUserNotificationString(forKey: "Tether", arguments: nil)
+//        content.body = NSString.localizedUserNotificationString(forKey: "Please keep app running for full alarm functionality", arguments: nil)
+//        content.sound = UNNotificationSound.default
+//
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+//        let request = UNNotificationRequest(identifier: "OneSecond", content: content, trigger: trigger) // Schedule the notification.
+//        let center = UNUserNotificationCenter.current()
+//        center.add(request) { (error : Error?) in
+//             if let theError = error {
+//                 // Handle any errors
+//                 print("\(theError)")
+//             }
+//        }
+        scheduleAppRefresh()
+        scheduleSentryWatch()
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("App active")
+        cancelAllPendingBGTask()
+    }
+    
+    private func registerBackgroundTasks() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.tether.sentry", using: nil) { task in
+//        self.handleImageFetcherTask(task: task as! BGProcessingTask)
+        }
 
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.tether.apprefresh", using: nil) { task in
+            self.handleAppRefresh(task: task as! BGAppRefreshTask)
+        }
+    }
+    
+    func cancelAllPendingBGTask() {
+        BGTaskScheduler.shared.cancelAllTaskRequests()
+    }
+    
+    func handleAppRefresh(task: BGAppRefreshTask) {
+       // Schedule a new refresh task.
+        print("handling refresh")
+       scheduleAppRefresh()
+
+       // Create an operation that performs the main part of the background task.
+//       let operation = RefreshAppContentsOperation()
+//
+//       // Provide the background task with an expiration handler that cancels the operation.
+//       task.expirationHandler = {
+//          operation.cancel()
+//       }
+//
+//       // Inform the system that the background task is complete
+//       // when the operation completes.
+//       operation.completionBlock = {
+//          task.setTaskCompleted(success: !operation.isCancelled)
+//       }
+//
+//       // Start the operation.
+//       OperationQueue.addOperation(operation)
+     }
+    
+    func scheduleAppRefresh() {
+       let request = BGAppRefreshTaskRequest(identifier: "com.tether.apprefresh")
+       // Fetch no earlier than 5 seconds from now.
+       request.earliestBeginDate = Date(timeIntervalSinceNow: 2 * 5)
+            
+       do {
+          try BGTaskScheduler.shared.submit(request)
+       } catch {
+          print("Could not schedule app refresh: \(error)")
+       }
+    }
+    
+    func scheduleSentryWatch() {
+        let request = BGProcessingTaskRequest(identifier: "com.tether.sentry")
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 5) // set next task no earlier than 5 seconds from now
+        
+        do {
+           try BGTaskScheduler.shared.submit(request)
+        } catch {
+           print("Could not schedule sentry watch: \(error)")
+        }
+    }
+    
+    
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+          .requestAuthorization(
+            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            self?.getNotificationSettings()
+          }
+    }
+
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+//        print("Notification settings: \(settings)")
+      }
+    }
 
 }
 
--- tether/ViewController.swift
+++ tether/ViewController.swift
@@ -6,14 +6,218 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
+import Gifu
+import BackgroundTasks
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var plugStatusLbl: UILabel!
+    @IBOutlet weak var secureBtn: UIButton!
+    @IBOutlet weak var plugImgView: UIImageView!
+    @IBOutlet weak var eyesGifView: GIFImageView!
+    @IBOutlet weak var optionsBtn: UIButton!
+    
+    let center = NotificationCenter.default
+    var token: NSObjectProtocol?
+    var bkgnd: NSObjectProtocol?
+    
+    var player: AVAudioPlayer?
+    var currSound: String = "frozen"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        setPlugImage()
+        setPopupButton()
+        eyesGifView.contentMode = .scaleAspectFit
+        eyesGifView.prepareForAnimation(withGIFNamed: "eye-gif")
+        token = center.addObserver(forName: UIDevice.batteryStateDidChangeNotification, object: nil, queue: nil, using: { note in
+            self.setPlugImage()
+            self.shouldTrigger()
+        })
+        
+        bkgnd = center.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil, using: { note in
+//            self.sendAlert(message: "Please keep app running for full functionality.")
+            let content = UNMutableNotificationContent()
+            content.title = NSString.localizedUserNotificationString(forKey: "Tether", arguments: nil)
+            content.body = NSString.localizedUserNotificationString(forKey: "Keep app running for full alarm functionality", arguments: nil)
+            content.sound = UNNotificationSound.default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.5, repeats: false)
+            let request = UNNotificationRequest(identifier: "comeback", content: content, trigger: trigger) // Schedule the notification.
+            let center = UNUserNotificationCenter.current()
+            center.add(request) { (error : Error?) in
+                 if let theError = error {
+                     // Handle any errors
+                     print("\(theError)")
+                 }
+            }
+        })
     }
+    
+    @IBAction func secureBtnAction(_ sender: Any) {
+        if (statusLbl.text == "secured") {
+            disableAuthFlow()
+        } else {
+            switch UIDevice.current.batteryState.rawValue {
+            case 0:
+                // unknown
+                sendAlert(message: "Cannot enable alarm. Battery state unknown.")
+            
+            case 1:
+                // unplugged
+                sendAlert(message: "Plug device in to enable alarm.")
+                
+            case 2:
+                // plugged in
+                enable()
+                
+            case 3:
+                // plugged in and @ 100%
+                enable()
+                
+            default:
+                print("default case...should never reach here")
+            }
+        }
+    }
+    
+    func enable() {
+        toggleEyeAnimation()
+        statusLbl.text = "secured"
+        secureBtn.tintColor = .systemRed
+        secureBtn.setTitle("disable", for: .normal)
+        plugStatusLbl.text = ""
+    }
+    
+    func resetUI() {
+        toggleEyeAnimation()
+        statusLbl.text = "unsecured"
+        secureBtn.tintColor = .systemBlue
+        secureBtn.setTitle("enable", for: .normal)
+        setPlugImage()
+        playMusic()
+    }
+    
+    func disableAuthFlow(){
+        // TODO: two cases
+        // 1. if the phone was locked, then don't require face id / pass
+        // 2. if phone was NOT locked, then required authentication
+        // RN AM JUST SOLVING FOR MVP
+        let context = LAContext()
+        context.localizedCancelTitle = "Beta. Cancel will authenticate."
+        
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) else {
+            print(error?.localizedDescription ?? "Can't evaluate policy")
 
+            // Fall back to a asking for username and password.
+            // if can't use face id, auto authenticate
+            return
+        }
+        
+        Task {
+            do {
+                try await context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Disable alarm")
+                resetUI()
+            } catch let error {
+                print(error.localizedDescription)
+                sendAlert(message: "Authentication Failed. Try again.")
+                // Fall back to a asking for username and password.
+                // ...
+            }
+        }
+    }
+    
+    func sendAlert(title: String = "Error", message: String, completionMethod: @escaping () -> Void = {}) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("errored.")
+        }))
+        self.present(alert, animated: true, completion: completionMethod)
+    }
+    
+    func toggleEyeAnimation() {
+        if (eyesGifView.isAnimatingGIF) {
+            eyesGifView.stopAnimatingGIF()
+            eyesGifView.prepareForAnimation(withGIFNamed: "eye-gif")
+        } else {
+            eyesGifView.animate(withGIFNamed: "eye-gif")
+        }
+    }
+    
+    /// Set image of plug based on
+    func setPlugImage() {
+        let curr = UIDevice.current.batteryState.rawValue
+        
+        if (curr == 1) {
+            plugImgView.image = UIImage(systemName: "powerplug")
+            plugImgView.tintColor = .systemGray
+            plugStatusLbl.text = "plug in to enable alarm."
+        } else if (curr == 2 || curr == 3) {
+            plugImgView.image = UIImage(systemName: "powerplug.fill")
+            plugImgView.tintColor = .systemGreen
+            plugStatusLbl.text = "ready to enable."
+        } else {
+            plugImgView.image = UIImage(systemName: "xmark.circle.fill")
+            plugImgView.tintColor = .systemRed
+            plugStatusLbl.text = "error: unknown battery state."
+        }
+    }
+    
+    func shouldTrigger() {
+        // check alarm condition
+        if (statusLbl.text == "secured" && UIDevice.current.batteryState.rawValue == 1) {
+            // start sounds & vibrations
+            playMusic()
+//            AudioServicesPlayAlertSound(SystemSoundID(1304))
+            sendAlert(title: "Alert", message: "Alarm triggered.")
+
+        }
+    }
+    
+    func playMusic() {
+        if let player = player, player.isPlaying || statusLbl.text == "unsecured" { // if disable sound btn clicked after audio ends -->
+            // stop playback
+            player.stop()
+        } else {
+            // set up player, and play
+            let audioData = NSDataAsset(name: currSound)
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                guard let audioData = audioData else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(data: audioData.data)
+                guard let player = player else {
+                    return
+                }
+                
+                player.numberOfLoops = -1
+                
+                player.play()
+            }
+            catch {
+                print("sth went wrong")
+            }
+        }
+    }
+    
+    func setPopupButton(){
+        let optionClosure = {(action : UIAction) in print(action.title)
+            self.currSound = action.title
+        }
+        optionsBtn.menu = UIMenu(children: [ UIAction(title: "frozen", handler: optionClosure), UIAction(title: "beeps", handler: optionClosure), UIAction(title: "piercing", handler: optionClosure), UIAction(title: "ishil", handler: optionClosure)])
+        optionsBtn.showsMenuAsPrimaryAction = true
+        optionsBtn.changesSelectionAsPrimaryAction = true
+    }
 
 }
 
